15a215d779e126d42cfbbc268d978b97446f81a4
Jose Mejias <josemejias081@gmail.com>
2021-09-02 15:33:27 +0200

    modify route home

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   
   devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/welcome", to: "home#index"
+  #get "/welcome", to: "home#index"
 
   get '/users/my_topics', to: 'users#my_topics', as: "my_topics"
   get '/users/my_categories', to: 'users#my_categories', as: "my_categories"
