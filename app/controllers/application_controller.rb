efe932f813e70864437e14df45fcccda4cedce58
Jose Mejias <josemejias081@gmail.com>
2021-09-24 18:05:02 +0200

    modified welcome page and sign_in & sign_up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
   include Pagy::Backend
+  #before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root to: "home#index"
  
+  #devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users
   
-  devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #get "/welcome", to: "home#index"
 
@@ -9,7 +11,7 @@ Rails.application.routes.draw do
   get '/users/my_categories', to: 'users#my_categories', as: "my_categories"
   
 
-  root to: 'home#index'
+  
 
   resources :categories
 
